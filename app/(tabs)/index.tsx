1066dd32639e8b0047c354a38ef60b68d9f5ecf1
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-10-02 19:18:34 +0200

    update index tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const IndexScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Witaj w mojej aplikacji!</Text>
+      <Text style={styles.title}>Witaj w mojej aplikacji!!!!!!!!!!!!!!!!!!</Text>
     </View>
   );
 };
